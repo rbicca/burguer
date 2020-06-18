0886c401a0eef8d11977f859b658a5ada2931d7a
Ronaldo Melchiades Bicca <ronaldo@softkuka.com.br>
2020-06-17 23:52:24 -0300

    Toolbar e logo iniciado

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Bag from '../../hoc/Bag';
 import classes from './Layout.module.css';
+import Toobar from '../Naviagation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Bag>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toobar />
         <main className={classes.Content}>
             {props.children}
         </main>
