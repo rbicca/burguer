bc7ba4c20d315b7e0e70addc1fb423eed8845f5c
Ronaldo Melchiades Bicca <ronaldo@softkuka.com.br>
2020-07-01 19:07:48 -0300

    Finalização projeto

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Bag from '../../hoc/Bag';
 import classes from './Layout.module.css';
 import Toobar from '../Naviagation/Toolbar/Toolbar';
 import SideDrawer from '../Naviagation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Bag>
-        <Toobar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Bag>
-);
+const layout = (props) => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [showSideDrawer, setShowSideDrawer] = useState(true);
+
+    const sideDrawerClosedHandler = () =>{
+        setShowSideDrawer(false);
+    }
+
+    const sideDrawerToggleHandler = () => {
+        console.log('oi');
+        setShowSideDrawer(!showSideDrawer);
+    }
+
+    return(
+            <Bag>
+                <Toobar drawerToggleClicked={sideDrawerToggleHandler}/>
+                <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {props.children}
+                </main>
+            </Bag>      
+    );
+}
 
 export default layout;
--- src/components/Naviagation/SideDrawer/SideDrawer.js
+++ src/components/Naviagation/SideDrawer/SideDrawer.js
@@ -3,18 +3,26 @@ import classes from './SideDrawer.module.css';
 
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItens/NavItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Bag from '../../../hoc/Bag';
 
 const sideDrawer = (props) => {
 
-
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        <Bag>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height="11%"/>
+                <nav>
+                    <NavItems />
+                </nav>
+            </div>
+        </Bag>
     );
 }
 
